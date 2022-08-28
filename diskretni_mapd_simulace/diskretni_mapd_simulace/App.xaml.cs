d0659814cfd6ba0a25c06a5d88bf6e7731aea54d
kwimbik <kwimbiks@gmail.com>
2022-08-28 19:22:53 +0200

    init projektu

---
+++ diskretni_mapd_simulace/diskretni_mapd_simulace/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace diskretni_mapd_simulace
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
