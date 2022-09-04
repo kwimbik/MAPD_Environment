da8943a28c9e7f8d58f44f92858db0ba3d8a0622
kwimbik <kwimbiks@gmail.com>
2022-09-04 12:30:11 +0200

    zakladni kostry trid pro auta, lokace a objednavky

---
+++ diskretni_mapd_simulace/diskretni_mapd_simulace/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diskretni_mapd_simulace
+{
+    public class Location
+    {
+        int id;
+        int[] coordination = new int[2];
+    }
+}
